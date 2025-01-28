aa16397ba602a58f3881306b80be783c7eeb27c2
Nick Plekhanov <nicksp@users.noreply.github.com>
2025-01-28 21:14:37 +0100

    Setup Scalar API reference

--- src/lib/configure-open-api.ts
+++ src/lib/configure-open-api.ts
@@ -1,13 +1,30 @@
+import { apiReference } from '@scalar/hono-api-reference'
+
 import type { AppOpenAPI } from './types.js'
 
 import packageJSON from '../../package.json' assert { type: 'json' }
 
+const openAPIPath = '/doc'
+
 export default function configureOpenAPI(app: AppOpenAPI) {
-  app.doc('/doc', {
+  app.doc(openAPIPath, {
     openapi: '3.0.0',
     info: {
       version: packageJSON.version,
       title: 'REST API',
     },
   })
+
+  app.get('/reference', apiReference({
+    theme: 'kepler',
+    layout: 'classic',
+    defaultHttpClient: {
+      targetKey: 'javascript',
+      clientKey: 'fetch',
+    },
+    pageTitle: 'REST API Reference',
+    spec: {
+      url: openAPIPath,
+    },
+  }))
 }
