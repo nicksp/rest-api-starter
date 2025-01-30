dc7b06cbcdc42aa50406f52a1f40ab6d6af9eec0
Nick Plekhanov <nicksp@users.noreply.github.com>
2025-01-30 04:01:55 +0100

    Setup Drizzle

---
+++ src/db/migrate.ts
@@ -0,0 +1,15 @@
+import { drizzle } from 'drizzle-orm/postgres-js'
+import { migrate } from 'drizzle-orm/postgres-js/migrator'
+
+import dbConfig from '@/../drizzle.config.js'
+
+import { migrationClient } from './index.js'
+
+async function run() {
+  await migrate(drizzle(migrationClient), { migrationsFolder: dbConfig.out! })
+
+  // Don't forget to close the connection, otherwise the script will hang
+  await migrationClient.end()
+}
+
+run()
