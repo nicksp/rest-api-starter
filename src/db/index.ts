e9849ceec11a7e1e1701c05ad6b4ee81af8f6be6
Nick Plekhanov <nicksp@users.noreply.github.com>
2025-02-01 05:22:43 +0100

    Update Drizzle stuff according to latest developments

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,4 +6,4 @@ import env from '@/env.js'
 export const migrationClient = postgres(env.DATABASE_URL, { max: 1 })
 export const queryClient = postgres(env.DATABASE_URL)
 
-export const db = drizzle({ client: queryClient })
+export const db = drizzle({ client: queryClient, casing: 'snake_case' })
