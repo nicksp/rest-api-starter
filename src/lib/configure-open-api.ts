d6d5d53c5c9cf56e6b2434b9dbcf8579191a5d52
Nick Plekhanov <nicksp@users.noreply.github.com>
2025-02-01 05:13:31 +0100

    Update API reference metadata for rich output

--- src/lib/configure-open-api.ts
+++ src/lib/configure-open-api.ts
@@ -1,5 +1,7 @@
 import { apiReference } from '@scalar/hono-api-reference'
 
+import env from '@/env.js'
+
 import type { AppOpenAPI } from './types.js'
 
 import packageJSON from '../../package.json' assert { type: 'json' }
@@ -11,7 +13,8 @@ export default function configureOpenAPI(app: AppOpenAPI) {
     openapi: '3.0.0',
     info: {
       version: packageJSON.version,
-      title: 'REST API',
+      title: 'Product API',
+      description: 'The Product API allows for managing tasks. It is based on the OpenAPI 3.0 specification. You can find out more about Swagger at <http://swagger.io>.',
     },
   })
 
@@ -19,12 +22,16 @@ export default function configureOpenAPI(app: AppOpenAPI) {
     '/reference',
     apiReference({
       theme: 'kepler',
-      layout: 'classic',
+      servers: [
+        { url: `http://localhost:${env.PORT}`, description: 'Local server' },
+        { url: 'https://api.product.com/v1', description: 'Production server' },
+      ],
       defaultHttpClient: {
         targetKey: 'js',
         clientKey: 'fetch',
       },
-      pageTitle: 'REST API Reference',
+      pageTitle: 'Product API Documentation',
+      hideClientButton: true,
       spec: {
         url: openAPIDocPath,
       },
